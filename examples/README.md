343ce33d63f1dd9217889455fe531d71df32c6f0
PatricioClark <patoclark@gmail.com>
2025-11-11 11:24:05 -0300

    Fixed example names

--- examples/README.md
+++ examples/README.md
@@ -2,9 +2,9 @@ Examples and use cases.
 
 - test_derivative:
     - Tests derivatives in 1- and 2D
-- time_marching_ks1D:
+- kse:
     - Forward evolution of 1D Kuramoto-Sivashinsky equation
-- time_marching_kolmogorov:
+- kolmogorov:
     - Forward evolution of 2D Kolmogorov flow
 - newton_kolmog: 
     - Example on Kolmogorov flow, orb05
---
+++ examples/kse/time_marching.py
@@ -0,0 +1,34 @@
+'''
+Pseudo-spectral solver for the 1D Kuramoto-Sivashinsky equation
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+import spooky as sp
+from spooky.solvers import KuramotoSivashinsky
+
+import params as pm
+pm.Lx *= 2*np.pi
+
+#  Initialize solver
+grid   = sp.Grid1D(pm)
+solver = KuramotoSivashinsky(pm)
+
+# Initial conditions
+uu = (0.3*np.cos(2*np.pi*3.0*grid.xx/pm.Lx) +
+    0.4*np.cos(2*np.pi*5.0*grid.xx/pm.Lx) +
+    0.5*np.cos(2*np.pi*4.0*grid.xx/pm.Lx)
+    )
+fields = [uu]
+
+# Evolve
+fields = solver.evolve(fields, T=pm.Tevolve, bstep=pm.bstep, ostep=pm.ostep)
+
+# Plot Balance
+bal = np.loadtxt('balance.dat', unpack=True)
+
+plt.figure()
+plt.plot(bal[0], bal[1])
+plt.savefig('balance.png', dpi = 300)
+plt.close()    
