5c9c70c8f3bac9671361379a0f8d3746230382b1
Joaquin Cullen <joaquincullen@gmail.com>
2025-11-17 10:38:14 -0300

    Fixed GHOST time marching example

--- examples/ghost_wrapper/time_marching_kolmog/run_evol.py
+++ examples/ghost_wrapper/time_marching_kolmog/run_evol.py
@@ -4,34 +4,18 @@ GHOST wrapper example for Kolmogorov Flow
 
 import numpy as np
 import matplotlib.pyplot as plt
-import os
 
-import spooky as ps
+import spooky as sp
 from spooky.solvers import GHOST
-from spooky.methods import DynSys
 import params as pm
 
-
-pm.Lx = 2*np.pi*pm.L
-pm.Ly = 2*np.pi*pm.L
-
 # Initialize solver
-grid = ps.Grid2D(pm)
-solver = GHOST(pm)
-lyap = DynSys(pm, solver)
+grid = sp.Grid2D(Lx=pm.Lx, Ly=pm.Ly, Nx=pm.Nx, Ny=pm.Ny, dt=pm.dt)
+solver = GHOST(grid, nu=pm.nu, nprocs=pm.nprocs, precision=pm.precision, ext=pm.ext)
 
-
-# If loading initial velocity field
-# path = 'input'
-# fields = solver.load_fields(path, 0)
-
-# If writing initial velocity field
+# Initial velocity field
 uu = np.cos(2*np.pi*1.0*grid.yy/pm.Lx) + 0.1*np.sin(2*np.pi*2.0*grid.yy/pm.Lx)
 vv = np.cos(2*np.pi*1.0*grid.xx/pm.Lx) + 0.2*np.cos(3*np.pi*2.0*grid.yy/pm.Lx)
-# Null initial velocity field
-# uu = np.zeros_like(grid.xx)
-# vv = np.zeros_like(grid.xx)
-
 
 fields = [uu, vv]
 #Apply solenoidal projection
@@ -40,28 +24,23 @@ ffields = grid.inc_proj(ffields)
 fields_i = [grid.inverse(ff) for ff in ffields]
 
 # If you only need last fields
-# fields = solver.evolve(fields_i, pm.T)
+# fields = solver.evolve(fields_i, pm.Tevolve)
 
 # If you need intermediate fields
-fields = solver.evolve(fields, pm.T, pm.ipath, pm.opath, pm.bstep, pm.ostep, pm.sstep, pm.bpath, pm.spath)
+fields = solver.evolve(fields, pm.Tevolve, opath=pm.opath, ostep=pm.ostep, bstep=pm.bstep)
 
 # Plot initial and final fields
-for field_i, field, ftype in zip(fields_i, fields, solver.ftypes):
-    fig = plt.figure(figsize = (12, 6))
+for field_i, field_f, ftype in zip(fields_i, fields, solver.ftypes):
+    fig = plt.figure(figsize = (10, 5))
 
-    plt.subplot(1,3,1)
+    plt.subplot(1,2,1)
     plt.imshow(field_i.T, origin='lower', cmap='viridis')
     plt.colorbar()
     plt.title(ftype)
 
-    plt.subplot(1,3,2)
-    plt.imshow(field.T, origin='lower', cmap='viridis')
+    plt.subplot(1,2,2)
+    plt.imshow(field_f.T, origin='lower', cmap='viridis')
     plt.colorbar()
     plt.title(ftype)
-
-    plt.subplot(1,3,3)
-    plt.imshow((field_i-field).T, origin='lower', cmap='viridis')
-    plt.colorbar()
-    plt.title(ftype)
-    plt.savefig(os.path.join(pm.opath,f'{ftype}.png'))
+    plt.savefig(f'{ftype}.png', dpi = 300)
     plt.show()
