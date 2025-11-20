42d4f19e894907a6d78434655d8d3df669b9e9f1
PatricioClark <patoclark@gmail.com>
2025-11-20 09:31:25 -0300

    Changed version number to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name='spookyflows',
-        version='0.2',
+        version='0.2.1',
         packages=find_packages(),
         description='Tools and algorithms for fluid flow simulations',
         url='https://github.com/PatricioClark/spooky',
